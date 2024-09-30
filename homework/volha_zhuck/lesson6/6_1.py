29b203a55eac6d70745e8c3ed74459b5f0a6ee70
olga <volha.zhuck@gmail.com>
2024-09-30 07:47:40 -0700

    2 files added for lesson6

---
+++ homework/volha_zhuck/lesson6/6_1.py
@@ -0,0 +1,12 @@
+s = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+words = s.split()
+punctuation = '.,?!'
+new_list = []
+for word in words:
+    if word[-1] in punctuation:
+        new_word = word[:-1] + "ing" + word[-1]
+    elif word[-1] not in punctuation:
+        new_word = word + "ing"
+    new_list.append(new_word)
+result = ' '.join(new_list)
+print(result)
