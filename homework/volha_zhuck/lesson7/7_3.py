d21b8d337a040a7453352c3ef8a1a502629bf573
olga <volha.zhuck@gmail.com>
2024-10-01 06:57:08 -0700

    corrected errors

--- homework/volha_zhuck/lesson7/7_3.py
+++ homework/volha_zhuck/lesson7/7_3.py
@@ -3,13 +3,15 @@ s2 = 'результат операции: 514'
 s3 = 'результат работы программы: 9'
 s4 = 'результат: 2'
 list = [s1, s2, s3, s4]
+
 def plus10(s):
-    lengh = len(s)
     ind = s.index(':')
     num = int(s[ind + 1:])
     result = num + 10
     print(result)
+
 def getnums(list):
     for str in list:
         plus10(str)
+        
 getnums(list)
