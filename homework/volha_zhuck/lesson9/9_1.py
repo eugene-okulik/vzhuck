2c4890bd778c5b9674aacd1182fba237d872abaa
olga <volha.zhuck@gmail.com>
2024-10-03 07:44:07 -0700

    corrected errors in 2 files for lesson9

--- homework/volha_zhuck/lesson9/9_1.py
+++ homework/volha_zhuck/lesson9/9_1.py
@@ -3,6 +3,6 @@ import datetime
 my_date = 'Jan 15, 2023 - 12:05:33'
 python_date = datetime.datetime.strptime(my_date, '%b %d, %Y - %I:%M:%S')
 human_month = python_date.strftime('%B')
-new_date = python_date.strftime('%d.%m.%Y, %I:%M') #Распечатайте дату в таком формате: "15.01.2023, 12:05"
+new_date = python_date.strftime('%d.%m.%Y, %I:%M')  # Распечатайте дату в таком формате: "15.01.2023, 12:05"
 print(human_month)
 print(new_date)
