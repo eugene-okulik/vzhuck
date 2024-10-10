b572d0aed4af95976d322da795c06af39aac9a70
olga <volha.zhuck@gmail.com>
2024-10-09 20:34:29 -0400

    corrected errors

--- homework/volha_zhuck/lesson11/11_1.py
+++ homework/volha_zhuck/lesson11/11_1.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 
+
 class Book:
     material_pages = "бумага"
     text = False
@@ -8,9 +9,7 @@ class Book:
     pages = 0
     ISBN = ''
     reserv = False
-
-
-    def __init__(self, material_pages, text, book_name, author, pages, reserv = False) -> None:
+    def __init__(self, material_pages, text, book_name, author, pages, reserv=False) -> None:
         self.material_pages = material_pages
         self.text = text
         self.book_name = book_name
@@ -18,39 +17,47 @@ class Book:
         self.pages = pages
         self.reserv = reserv
         pass
-
-
     @abstractmethod
     def printBook(self):
         if self.reserv:
-            print(f"Название: {self.book_name}, Автор: {self.author}, страниц: {self.pages}, материал: {self.material_pages}, зерезервирована")
+            print(
+            f"Название: {self.book_name}, Автор: {self.author}, "
+            f"страниц: {self.pages}, материал: {self.material_pages}, "
+            "зерезервирована"
+        )
         else:
-            print(f"Название: {self.book_name}, Автор: {self.author}, страниц: {self.pages}, материал: {self.material_pages}")
+            print(
+            f"Название: {self.book_name}, Автор: {self.author}, "
+            f"страниц: {self.pages}, материал: {self.material_pages}"
+        )
 
 
 class SchoolBooks(Book):
     lesson = ''
     grade = ''
     practice = True
-
-
-    def __init__(self, material_pages, text, book_name, author, pages, lesson, grade, practice, reserv = False) -> None:
+    def __init__(self, material_pages, text, book_name, author, pages, lesson, grade, practice, reserv=False) -> None:
         super().__init__(material_pages, text, book_name, author, pages, reserv)
         self.lesson = lesson
         self.grade = grade
         self.practice = practice
-
-
     def printBook(self):
         if self.reserv:
-            print(f"Название: {self.book_name}, Автор: {self.author}, страниц: {self.pages}, предмет: {self.lesson}, класс: {self.grade}, зерезервирована")
+            print(
+            f"Название: {self.book_name}, Автор: {self.author}, "
+            f"страниц: {self.pages}, предмет: {self.lesson}, класс: {self.grade}, "
+            "зерезервирована"
+        )
         else:
-            print(f"Название: {self.book_name}, Автор: {self.author}, страниц: {self.pages}, предмет: {self.lesson}, класс: {self.grade}")
+            print(
+            f"Название: {self.book_name}, Автор: {self.author}, "
+            f"страниц: {self.pages}, предмет: {self.lesson}, класс: {self.grade}"
+        )
 
 
 book1 = Book('бумага', True, 'Идиот', 'Достоевский', 500)
 book1.printBook()
-book2 = Book('бумага', True, 'Преступление и наказание', 'Достоевский', 300 , True)
+book2 = Book('бумага', True, 'Преступление и наказание', 'Достоевский', 300, True)
 book2.printBook()
 book3 = Book('глянец', True, 'Мертывые души', 'Чехов', 450)
 book3.printBook()
